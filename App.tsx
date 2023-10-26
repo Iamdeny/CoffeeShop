687f9738175213b396fc128569bd17ef3c3f0f07
hellodeny <denchakuznec@gmail.com>
2023-10-26 16:41:09 +0900

    innitial commit

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import DetailsScreen from './src/screens/DetailsScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+import SplashScreen from 'react-native-splash-screen';
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
+
+
+
+useEffect(() => {
+  const unsubscribe = messaging().onMessage(async remoteMessage => {
+    Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+  });
+
+  return unsubscribe;
+}, []);
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{ animation: 'slide_from_bottom' }}></Stack.Screen>
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{ animation: 'slide_from_bottom' }}></Stack.Screen>
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{ animation: 'slide_from_bottom' }}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
